9afa1bd3ab3859b1a0ba06887a02c2f7ba36f31c
NikolayRag <thenickra@gmail.com>
2024-10-27 20:51:20 +0300

    refactor tabs

--- src/kThing/ktChar.py
+++ src/kThing/ktChar.py
@@ -8,51 +8,63 @@ Character is responsible of providing user-specific settings, conversations and 
 It is specifically focused on impersonated AI behavior - interpreting and response.
 '''
 class ktChar():
-   uId = 0
+	uId = 0
 
-   firstName = ''
-   lastName = ''
-   nickName = ''
-   lang = 'en'
+	firstName = ''
+	lastName = ''
+	nickName = ''
+	lang = 'en'
 
-   defHello: 'hi'
-
-
-
-
-   def __init__(self, _id):
-   	self.uId = _id
-
-
-   def setup(self, firstName='', lastName='', nickName='', lang=''):
-      self.firstName = firstName
-      self.lastName = lastName
-      self.nickName = nickName
-      self.lang = lang
-
-
-   def getId(self):
-      return self.uId
+	defHello: 'hi'
 
 
 
-   def getLang(self):
-      return 'ru'
 
 
+
+	def __init__(self, _id):
+		self.uId = _id
+
+
+
+	def setup(self, firstName='', lastName='', nickName='', lang=''):
+		log.info(f"Start user {nickName}")
+
+		self.firstName = firstName
+		self.lastName = lastName
+		self.nickName = nickName
+		self.lang = lang
+
+
+
+	def getId(self):
+		return self.uId
+		
 
    # -todo 40 (interact, char) +0: compose hello
-   def getHello(self):
-      return f"Hi, {self.firstName}"
+
+
+	def getLang(self):
+		return 'ru'
 
 
 
-   def addQ(self, _msg):
-      return
+	def getHello(self):
+		return f"Hi, {self.firstName}"
 
 
-   def addA(self, _msg):
-      return
 
+	'''
+	Add message to personal history as user query
+	'''
+	def addQ(self, _msg):
+		return
+
+
+	'''
+	Add message to personal history as AI answer
+	'''
+	def addA(self, _msg):
+		return
 
 
