0ffa94cd152b38bb1ea828c74aa37b08a6c804a3
NikolayRag <thenickra@gmail.com>
2024-10-31 12:47:50 +0300

    Fix accidentally off DB

--- src/kThing/support.py
+++ src/kThing/support.py
@@ -5,5 +5,5 @@ log.basicConfig(level=defLogLevel)
 
 from .kDB import *
 #  todo 32 (refactor, db) +0: db entrance
-#defDBLocation = 'db.sql'
-#kDB.init(defDBLocation)
+defDBLocation = 'db.sql'
+kDB.init(defDBLocation)
