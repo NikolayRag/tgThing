ac2c447db72a1373d681a6ea1174b532c0a6246f
NikolayRag <thenickra@gmail.com>
2024-11-07 04:29:13 +0300

    fix Fuzzb init

--- src/__main__.py
+++ src/__main__.py
@@ -1,12 +1,16 @@
 
 
 from kThing import *
+from kThing.tools.tool_fusionbrain import *
 
 
 
 keyAi = '123...'
 keyBot = '456...'
 
+fuzzApi = '234...'
+fuzzSecret = '345...'
+Fuzzb.setKeys(api_key=fuzzApi, secret_key=fuzzSecret)
 
 theThing = kThing(keyAi, keyBot)
 
